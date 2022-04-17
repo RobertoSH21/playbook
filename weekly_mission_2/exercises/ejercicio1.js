34bd68c6c05f2bb9510ad145b4c6045e267604ca
RobertoSH <al217053@alumnos.uacj.mx>
2022-04-16 19:57:05 -0600

    ejercicio 1

---
+++ weekly_mission_2/exercises/ejercicio1.js
@@ -0,0 +1,22 @@
+const issue = {
+    title: "Haciendo mi primer issue",
+    repositoryNameAssociated: "Prueba",
+    status: "",
+    numberOfComments: 50,
+    labels: "Empezando, Issue-1",
+    author: " Robert",
+    dateCreated: "16 abr 2022 20:06",
+    lastUpdated: "2 days ago",
+
+
+    getTitle: function(){
+      return this.title + this.author
+    },
+    getGeneralInfo: function(){
+      return `El repositorio asociado es: ${this.repositoryNameAssociated}`
+    }
+   }
+   
+   console.log("El titulo y el autor es: " + issue.getTitle())
+   console.log("El issue tiene un total de: " + issue.numberOfComments + " comentarios")
+   console.log(issue.getGeneralInfo())
