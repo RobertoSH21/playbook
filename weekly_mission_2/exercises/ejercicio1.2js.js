0238527689990980bf33dbcda62544885a13a274
RobertoSH <al217053@alumnos.uacj.mx>
2022-04-17 18:52:01 -0600

    terminando primera parte del ejercicio

--- weekly_mission_2/exercises/ejercicio1.2js.js
+++ weekly_mission_2/exercises/ejercicio1.2js.js
@@ -1,7 +1,19 @@
 const pullrequest = {
     title: "Duda",
+    author: " Robert",
     branchname: "Prueba",
     datecreated: "16 abr 2022 22:00",
     status: "nothing to commit, working tree clean",
-    repositoryNameAssociated: "Error de semana"
+    repositoryNameAssociated: "Error de semana",
+
+    getStatus: function(){
+        return this.status
+    },
+
+    getTitleAndAutor: function(){
+        return this.title + this.author
+    }
 } 
+
+console.log("El estado es: " + pullrequest.getStatus())
+console.log("El titulo y autor es: " + pullrequest.getTitleAndAutor())
