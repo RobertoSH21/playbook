8cf2591a72b2708dd6e8101a00280d19a3c442da
RobertoSH <al217053@alumnos.uacj.mx>
2022-04-17 19:32:38 -0600

    prueba con twitter

---
+++ weekly_mission_2/exercises/ejercico1.3.js
@@ -0,0 +1,89 @@
+/// Twitter
+/*
+--perfil--
+ *usuario
+ *username
+ *bio
+ *age
+ *seguidores
+ *seguidos
+ *creacion de cuenta
+ *editar perfil
+--Tweets--
+ *Tweet
+ *foto
+ *video
+ *gif
+--tweets y respuestas--
+ *foto
+ *video
+ *comentar
+ *retwitt
+ *Megusta
+ *compartir
+--Multimedia--
+ *foto
+ *video
+ *comentar
+ *retwitt
+ *Megusta
+ *compartir
+--Me gusta--
+ *foto
+ *video
+ *comentar
+ *retwitt
+ *Megusta
+ *compartir
+
+*/
+const usertwitter = {
+    usuario: "Roberto",
+    username: "RobertSH217",
+    bio: "Es bueno tener un amigo que este a tu lado siempre",
+    age: 18,
+    creacion: "Se unio en marzo de 2018",
+    seguidos: 42,
+    seguidores: 3
+}
+
+const Tweet = {
+    usuario: "Roberto",
+    username: "RobertSH217",
+    Tweet: "mensaje",
+    foto: "fotos del usuario",
+    video: "videos del usuario"
+}
+
+const TweetsyRes = {
+    usuario: "Roberto",
+    username: "RobertSH217",
+    foto: "fotos del usuario",
+    video: "videos del usuario",
+    comentario: "comentario x",
+    compartir: "compatir con ..."
+}
+
+const Multimedia = {
+    usuario: "Roberto",
+    username: "RobertSH217",
+    foto: "fotos del usuario",
+    video: "videos del usuario",
+    comentario: "comentario x",
+    compartir: "compatir con ..." 
+}
+
+const Megusta = {
+    usuario: "Roberto",
+    username: "RobertSH217",
+    foto: "fotos del usuario",
+    video: "videos del usuario",
+    comentario: "comentario x",
+    compartir: "compatir con ..." 
+}
+
+console.log(usertwitter)
+console.log(Tweet)
+console.log(TweetsyRes)
+console.log(Multimedia)
+console.log(Megusta)
